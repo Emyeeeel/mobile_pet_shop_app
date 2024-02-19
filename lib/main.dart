ae9da4b1fabeb8dccb2e8099b40b3a7f60e6292f
Emyeeeel <maryamielcatado@gmail.com>
2024-02-19 16:09:03 +0800

    added provider to dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:pet_shop_app/screens/catalog.dart';
 
 import 'screens/landing_page.dart';
 
---
+++ lib/screens/catalog.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CatalogPage extends StatefulWidget {
+  const CatalogPage({Key? key}) : super(key: key);
+
+  @override
+  _CatalogPageState createState() => _CatalogPageState();
+}
+
+class _CatalogPageState extends State<CatalogPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+      children: [],
+    ));
+  }
+}
